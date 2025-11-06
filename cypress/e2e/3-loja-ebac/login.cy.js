0986cba5aff985a037e54dcf5e4ad3082b9b3faf
Laura Tomazelli <lauratomazellidecastro@gmail.com>
2025-11-06 14:31:30 -0300

    Primeiro teste de login automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,13 @@
+/// <reference type="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it ('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('laurateste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, laurateste (não é laurateste? Sair)')
+    
+    })
+})
